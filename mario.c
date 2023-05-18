bd6cabc1821adf20723afb91f4112dfc00f4fa3b
Justin Li <111467072+jqlgit@users.noreply.github.com>
2023-05-18 03:10:26 +0000

    Wed, May 17, 2023, 10:10 PM -05:00

--- mario.c
+++ mario.c
@@ -3,13 +3,26 @@
 
 int main(void)
 {
+    // get size of grid
+    int n = get_size();
+
+    // print grid of bricks
+    print_grid(n);
+}
+
+int get_size(void)
+{
     int n;
     do
     {
         n = get_int("Size: ");
     }
     while (n < 1);
-    
+    return n
+}
+
+string print_grid(void)
+{
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
