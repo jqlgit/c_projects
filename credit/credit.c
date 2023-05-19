4af70f2b608a95e097e97126288122cb4aa9eaf6
Justin Li <111467072+jqlgit@users.noreply.github.com>
2023-05-19 03:07:28 +0000

    Thu, May 18, 2023, 10:07 PM -05:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,11 +1,9 @@
 #include <cs50.h>
 #include <stdio.h>
 
-string check_sum(long number)
+//string check_sum(long number)
 int main(void)
 {
-    do
-    {
-        int x = get_long("Number: ");
-    }
+    int x = 1920;
+    printf(x[0]);
 }
