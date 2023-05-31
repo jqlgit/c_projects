0350646d9d59758895cd3a17c9ce8f06d72e0b0a
Justin Li <111467072+jqlgit@users.noreply.github.com>
2023-05-31 04:02:25 +0000

    Tue, May 30, 2023, 11:02 PM -05:00

--- credit/credit.c
+++ credit/credit.c
@@ -53,3 +53,28 @@
         return 0;
     }
 
+// Get starting digits
+    long start = n;
+    do
+    {
+        start = start / 10;
+    }
+    while (start > 100);
+
+// Next check starting digits for card type
+    if ((start / 10 == 5) && (0 < start % 10 && start % 10 < 6))
+    {
+        printf("MASTERCARD\n");
+    }
+    else if ((start / 10 == 3) && (start % 10 == 4 || start % 10 == 7))
+    {
+        printf("AMEX\n");
+    }
+    else if (start / 10 == 4)
+    {
+        printf("VISA\n");
+    }
+    else
+    {
+        printf("INVALID\n");
+    }
