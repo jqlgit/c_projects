6eaecb8240b0d602d6b939bb4158e7293a2a6d25
Justin Li <111467072+jqlgit@users.noreply.github.com>
2023-05-31 03:30:04 +0000

    Tue, May 30, 2023, 10:30 PM -05:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,3 +1,13 @@
 #include <stdio.h>
 #include <cs50.h>
 
+// Get card number
+    long n = get_long("Number: ");
+    // Count length
+    int i = 0;
+    long cc = n;
+    while (cc > 0)
+    {
+        cc = cc / 10;
+        i++;
+    }
