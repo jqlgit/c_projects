dd5babd0b73b964dedad4bc7c834aff197748c16
Justin Li <111467072+jqlgit@users.noreply.github.com>
2023-05-31 04:31:46 +0000

    Tue, May 30, 2023, 11:31 PM -05:00

--- credit/credit.c
+++ credit/credit.c
@@ -12,42 +12,29 @@ int get_card_length(long long card_number)
     return length;
 }
 
-bool is_valid_checksum(long long n)
+bool is_valid_checksum(long long card_number, int length)
 {
-   int sum1 = 0;
-   int sum2 = 0;
-   long x = n;
-   int total = 0;
-   int mod1;
-   int mod2;
-   int d1;
-   int d2;
-   do
-   {
-       // Remove last digit and add to sum1
-       mod1 = x % 10;
-       x = x / 10;
-       sum1 = sum1 + mod1;
-       // Remove second last digit
-       mod2 = x % 10;
-       x = x / 10;
-       // Double second last digit and add digits to sum2
-       mod2 = mod2 * 2;
-       d1 = mod2 % 10;
-       d2 = mod2 / 10;
-       sum2 = sum2 + d1 + d2;
-   }
-   while (x > 0);
-   total = sum1 + sum2;
+    int sum = 0;
+    bool multiply = false;
 
+    while (card_number > 0)
+    {
+        int digit = card_number % 10;
+        if (multiply)
+        {
+            digit *= 2;
+            sum += digit / 10 + digit % 10;
+        }
+        else
+        {
+            sum += digit;
+        }
 
-// Next check Luhn Algorithm
-   if (total % 10 != 0)
-   {
-       printf("INVALID\n");
-       return 0;
-   }
+        card_number /= 10;
+        multiply = !multiply;
+    }
 
+    return sum % 10 == 0;
 }
 
 string get_card_type(long long card_number, int length)
@@ -78,7 +65,7 @@ int main(void)
     long long card_number = get_long("Number: ");
     int length = get_card_length(card_number);
 
-    if (is_valid_checksum(card_number))
+    if (is_valid_checksum(card_number, length))
     {
         string card_type = get_card_type(card_number, length);
         printf("%s\n", card_type);
