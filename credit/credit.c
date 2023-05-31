9aab6195086e4e272ea05d5bb05c7a8606833bec
Justin Li <111467072+jqlgit@users.noreply.github.com>
2023-05-31 03:56:34 +0000

    Tue, May 30, 2023, 10:56 PM -05:00

--- credit/credit.c
+++ credit/credit.c
@@ -11,9 +11,45 @@
         cc = cc / 10;
         i++;
     }
+
  // Check if length is valid
     if (i != 13 && i != 15 && i != 16)
     {
         printf("INVALID\n");
         return 0;
     }
+
+// Calculate checksum
+    int sum1 = 0;
+    int sum2 = 0;
+    long x = n;
+    int total = 0;
+    int mod1;
+    int mod2;
+    int d1;
+    int d2;
+    do
+    {
+        // Remove last digit and add to sum1
+        mod1 = x % 10;
+        x = x / 10;
+        sum1 = sum1 + mod1;
+        // Remove second last digit
+        mod2 = x % 10;
+        x = x / 10;
+        // Double second last digit and add digits to sum2
+        mod2 = mod2 * 2;
+        d1 = mod2 % 10;
+        d2 = mod2 / 10;
+        sum2 = sum2 + d1 + d2;
+    }
+    while (x > 0);
+    total = sum1 + sum2;
+
+// Next check Luhn Algorithm
+    if (total % 10 != 0)
+    {
+        printf("INVALID\n");
+        return 0;
+    }
+
